2118a5aad23aacb0e710ef054abab584fd3854d2
krrllee <krleposkok12@gmail.com>
2023-01-12 10:47:08 +0100

    Common class library

---
+++ Common/Complaint.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    [DataContract]
+    public class Complaint
+    {
+        [DataMember]
+        public string Id { get; set; }
+        [DataMember]
+        public string Username { get; set; }
+        [DataMember]
+        public string Message { get; set; }
+
+        public Complaint()
+        {
+
+        }
+
+    }
+}
---
+++ Common/ICityGovernment.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+
+    [ServiceContract]
+    public interface ICityGovernment
+    {
+        [OperationContract]
+        void SendComplaint(string message, byte[] sign);
+
+        [OperationContract]
+        void BanComplainer(string username);
+
+        [OperationContract]
+        void ForgiveComplainer(string username);
+
+        [OperationContract]
+        List<Complaint> GetComplaintsWithBannedWords();
+
+        [OperationContract]
+        bool IsSupervisor();
+
+
+
+    }
+}
+
