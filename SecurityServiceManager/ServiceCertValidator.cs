fd1b3cfec74567f9276c74b6e3be46a744d2b871
Djordje Markovic <djmarkovic01@gmail.com>
2023-01-12 19:44:05 +0100

    Create ServiceCertValidator.cs

---
+++ SecurityServiceManager/ServiceCertValidator.cs
@@ -0,0 +1,32 @@
+﻿using Common;
+using SecurityServiceManager;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Selectors;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecurityServiceManager
+{
+    public class ServiceCertValidator : X509CertificateValidator
+    {
+        public override void Validate(X509Certificate2 certificate)
+        {
+            var bannedUsers = XMLManager.ReadBannedUsers();
+            string[] name = certificate.Subject.Split(',');
+
+            foreach (var user in bannedUsers)
+            {
+                string[] certName = user.Username.Split(',');
+                if (name[0].Equals(certName[0]) || name[0].Equals(certificate.Issuer))
+                {
+                    throw new Exception("User is banned");
+                }
+            }
+
+        }
+    }
+}
