8804a1a21780ee4ecf81122032c8aa565757861c
krrllee <krleposkok12@gmail.com>
2023-01-12 11:02:31 +0100

    classes for banned users and banned words

---
+++ SecurityServiceManager/BannedWord.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecurityServiceManager
+{
+    public class BannedWord
+    {
+        public string Word { get; set; }
+
+        public BannedWord()
+        {
+
+        }
+    }
+}
